dc4fcac5d0b792d2bbb82a1966d1892f79a974e5
杨慧杰 <17532569639@163.com>
2022-05-26 13:48:05 +0800

    class字节码操作

--- day008/src/main/java/com/example/day008/MainActivity.java
+++ day008/src/main/java/com/example/day008/MainActivity.java
@@ -1,14 +1,53 @@
 package com.example.day008;
 
 import android.os.Bundle;
+import android.util.Log;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+import java.lang.reflect.Field;
 
+public class MainActivity extends AppCompatActivity{
+    @RequiresApi(api = 31)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        Class clazz = User.class;
+//        String packageName = clazz.getSimpleName();
+//        Log.i("TAG--", "onCreate: "+packageName);
+        try {
+            //通过字节码创建对象
+            Class<?> aClass = Class.forName("com.example.day008.User");
+            User user = (User) aClass.newInstance();
+//            user.name = "刘继权";
+//            System.out.println(user.name);
+
+            //获取所有的字段 （私有拿不到）
+//            Field[] fields = aClass.getFields();
+//            Log.i("TAG--",  fields.length+"");
+
+            //获取所有的声明的字段
+//            Field[] declaredFields = aClass.getDeclaredFields();
+//            Log.i("TAG--", declaredFields.length+"");
+
+            //根据指定的名称获取指定的字段
+            Field nameField = aClass.getField("name");
+            nameField.set(user,"张三");
+            Log.i("TAG--", user.name);
+            Field address = aClass.getDeclaredField("address");
+            address.setAccessible(true);
+            address.set(user,"哇哈哈工厂");
+            Log.i("TAG--", user.getAddress());
+
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
     }
 }
---
+++ day008/src/main/java/com/example/day008/User.java
@@ -0,0 +1,17 @@
+package com.example.day008;
+
+/**
+ * @Date 2022/5/26
+ */
+public class User {
+    public String name;
+    private String address;
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
