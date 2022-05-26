fc1a4e990b0458a17f30b48e9365d8b87d99955d
杨慧杰 <17532569639@163.com>
2022-05-26 16:26:36 +0800

    mua

--- day007/src/main/java/com/example/day007/main/MainActivity.java
+++ day007/src/main/java/com/example/day007/main/MainActivity.java
@@ -2,8 +2,6 @@ package com.example.day007.main;
 
 import android.util.Log;
 
-import androidx.annotation.NonNull;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
@@ -13,17 +11,12 @@ import com.example.day007.bean.Food;
 import com.example.day007.core.BaseActivity;
 import com.example.day007.dao.FoodDataBase;
 import com.example.day007.databinding.ActivityMainBinding;
-import com.fhz.utilsfhz.ApiResponse;
-import com.scwang.smart.refresh.layout.api.RefreshLayout;
-import com.scwang.smart.refresh.layout.listener.OnLoadMoreListener;
-
-import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends BaseActivity<MainViewModel, ActivityMainBinding> {
-    private List<Food> list = new ArrayList<>();
+    private final List<Food> list = new ArrayList<>();
     private FoodShowAdapter foodShowAdapter;
 
     @Override
@@ -37,12 +30,9 @@ public class MainActivity extends BaseActivity<MainViewModel, ActivityMainBindin
 
         mDataBinding.rec.setLayoutManager(new LinearLayoutManager(MainActivity.this));
         getData();
-        mDataBinding.refresh.setOnLoadMoreListener(new OnLoadMoreListener() {
-            @Override
-            public void onLoadMore(@NonNull @NotNull RefreshLayout refreshLayout) {
+        mDataBinding.refresh.setOnLoadMoreListener(refreshLayout -> {
 //                getData();
-                mDataBinding.refresh.finishLoadMore();
-            }
+            mDataBinding.refresh.finishLoadMore();
         });
 
         foodShowAdapter = new FoodShowAdapter(R.layout.item_show, list);
@@ -62,13 +52,10 @@ public class MainActivity extends BaseActivity<MainViewModel, ActivityMainBindin
     }
 
     public void getData(){
-        mViewModel.getFood().observe(MainActivity.this, new Observer<ApiResponse<List<Food>>>() {
-            @Override
-            public void onChanged(ApiResponse<List<Food>> listApiResponse) {
-                list.addAll(listApiResponse.data);
-                Log.i("size--", "onChanged: "+list.size());
-                foodShowAdapter.notifyDataSetChanged();
-            }
+        mViewModel.getFood().observe(MainActivity.this, listApiResponse -> {
+            list.addAll(listApiResponse.data);
+            Log.i("size--", "onChanged: "+list.size());
+            foodShowAdapter.notifyDataSetChanged();
         });
     }
 }
