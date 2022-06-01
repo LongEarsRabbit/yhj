dba0fc77295ad1f86920a6ae90ad9bb7e8d38602
杨慧杰 <17532569639@163.com>
2022-06-01 08:18:34 +0800

    new day13

--- day012/src/main/java/com/example/day012/User.java
+++ day012/src/main/java/com/example/day012/User.java
@@ -8,12 +8,19 @@ import javax.inject.Inject;
 public class User {
     public String name;
 
+    public String address;
 
     public User() {
+
     }
+
     @Inject
     public User(String name) {
         this.name = name;
     }
 
+    public User(String name, String address) {
+        this.name = name;
+        this.address = address;
+    }
 }
