bc5a2d6f4b8c0a6fedd0530b79317f9c0ca559e7
杨慧杰 <17532569639@163.com>
2022-06-08 09:34:54 +0800

    end start month exam

---
+++ day016/src/main/java/com/example/day016/MyView.java
@@ -0,0 +1,23 @@
+package com.example.day016;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+/**
+ * 时间:2022/6/6
+ * 简述: TODO
+ */
+public class MyView extends View {
+
+
+    public MyView(Context context) {
+        super(context);
+    }
+
+    public MyView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
